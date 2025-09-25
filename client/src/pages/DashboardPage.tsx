96a0e815fdf61d62633a640b1482c9a9a60a7b21
claytoncrispim <claytonrpcrispim@gmail.com>
2025-09-25 13:09:03 +0100

    fix(client): Remove unused React imports for production build

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
 import CreateLinkForm from '../components/CreateLinkForm';
 import apiClient from '../apiClient';
